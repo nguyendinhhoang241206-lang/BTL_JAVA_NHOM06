169a2018c499ec0e29971a2ae1bc414ada053648
12-tech-ntb <nguyentatbac1222006@gmail.com>
2026-05-28 16:45:25 +0700

    feat: Hoàn thành feauture xóa phim

--- Demo/src/service/MovieSearchService.java
+++ Demo/src/service/MovieSearchService.java
@@ -14,8 +14,14 @@ public class MovieSearchService {
     public List<Movie> findAllMovies() {
         // TẠM ẨN DÒNG NÀY ĐỂ CHỜ LÀM XONG FILE .DAT
         // return movieDAO.findAll(); 
+        List<Movie> testList = new java.util.ArrayList<>();
+        testList.add(new Movie("M01", "Mai", "Phim rạp Trấn Thành", "Trấn Thành", 120, java.time.LocalDate.of(2024, 2, 10)));
+        testList.add(new Movie("M02", "Lật Mặt 7", "Một điều ước", "Lý Hải", 135, java.time.LocalDate.of(2024, 4, 26)));
+        testList.add(new Movie("M03", "Đào, Phở và Piano", "Phim lịch sử", "Phi Tiến Sơn", 100, java.time.LocalDate.of(2024, 2, 10)));
+        
+        return testList;
 
-        return null;
+//        return null;
     }
 
     // Hàm tìm kiếm
