49f78346fab35f9399bd4b0374a5233ac05304fb
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-06-17 22:45:33 +0700

    commit 6

--- Demo/src/controller/ListFavoriteController.java
+++ Demo/src/controller/ListFavoriteController.java
@@ -16,15 +16,26 @@ public class ListFavoriteController {
     }
 
     public List<Movie> getMyFavoriteMovies() {
+        try {
+            if (!Session.isLoggedIn()) {
+                System.err.println("Cảnh báo: Khách hàng chưa đăng nhập!");
+                return new ArrayList<>();
+            }
 
-        if (!Session.isLoggedIn()) {
-            System.out.println("Vui lòng đăng nhập!");
-            return new ArrayList<>();
+            User currentUser = Session.getCurrentUser();
+            
+            if (currentUser == null) {
+                System.err.println("Lỗi: Phiên đăng nhập bị hỏng không lấy được thông tin User.");
+                return new ArrayList<>();
+            }
+
+            List<Movie> favoriteMovies = listFavoriteService.getFavoriteMoviesByUser(currentUser);
+            
+            return (favoriteMovies != null) ? favoriteMovies : new ArrayList<>();
+            
+        } catch (Exception e) {
+            System.err.println("Lỗi hệ thống khi tải danh sách phim yêu thích: " + e.getMessage());
+            return new ArrayList<>(); 
         }
-
-
-        User currentUser = Session.getCurrentUser();
-
-        return listFavoriteService.getFavoriteMoviesByUser(currentUser);
     }
 }
